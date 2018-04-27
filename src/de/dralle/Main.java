8de21b40a39dd73433c777765688bb55ff5d8d78
Nils Dralle <nils.91@googlemail.com>
2018-04-27 10:59:48 +0200

    Added example from Vorlesung

--- src/de/dralle/Main.java
+++ src/de/dralle/Main.java
@@ -13,7 +13,7 @@ import javax.swing.JFrame;
 
 public class Main {
 	static Dimension defaultD=new Dimension(800, 600);
-	static Point coordinateSystemOriginOnScreen=new Point(50, 480);
+	static Point coordinateSystemOriginOnScreen=new Point(400, 300);
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		JFrame f=new JFrame();
@@ -25,22 +25,35 @@ public class Main {
 		//
 		List<Point> pointList=new ArrayList();
 		
-		pointList.add(new Point(0, 1));
-		pointList.add(new Point(1, 1));
-		pointList.add(new Point(2, 4));
-		pointList.add(new Point(3, 8));
-		pointList.add(new Point(4, 16));
-		pointList.add(new Point(5, 32));
-		pointList.add(new Point(6, 64));
-		pointList.add(new Point(7, 128));
-		pointList.add(new Point(8, 256));
+//		pointList.add(new Point(0, 1));
+//		pointList.add(new Point(1, 1));
+//		pointList.add(new Point(2, 4));
+//		pointList.add(new Point(3, 8));
+//		pointList.add(new Point(4, 16));
+//		pointList.add(new Point(5, 32));
+//		pointList.add(new Point(6, 64));
+//		pointList.add(new Point(7, 128));
+//		pointList.add(new Point(8, 256));
+		
+		pointList.add(new Point(-5, -8));
+		pointList.add(new Point(-4, -4));
+		pointList.add(new Point(-3, -6));
+		pointList.add(new Point(-2, -2));
+		pointList.add(new Point(-1, -3));
+		pointList.add(new Point(0, 2));
+		pointList.add(new Point(1, 4));
+		pointList.add(new Point(2, 1));
+		pointList.add(new Point(3, 3));
+		pointList.add(new Point(4, 6));
+		pointList.add(new Point(5, 6));
+		
 		
 		DrawPane drawPane = new DrawPane();
 		drawPane.setCoordinateSystemOriginOnScreen(coordinateSystemOriginOnScreen);
 		drawPane.setCoordinateSystemInternalOrigin(new Point(0, 0));
-		drawPane.setCoordinateSystemInternalPosSize(new Dimension(10, 300));
-		drawPane.setxScale(1);
-		drawPane.setyScale(25);
+		drawPane.setCoordinateSystemInternalPosSize(new Dimension(6, 8));
+		drawPane.setxScale(2);
+		drawPane.setyScale(2);
 		drawPane.setPointList(pointList);
 		f.add(drawPane);
 	}
